bdb15b24ee573501acd78e3cc87109a33d3f2a1b
jonathan <jonathan@jonathon>
2014-06-04 17:52:12 -0400

    j

--- src/cl/paradigmas/gui/eventos/EventBuilder.java
+++ src/cl/paradigmas/gui/eventos/EventBuilder.java
@@ -5,7 +5,20 @@ package cl.paradigmas.gui.eventos;
 final public class EventBuilder {
 private EventBuilder(){
 	
-	//button.addActionListener(this)
+	/*button.addActionListener(this)
+	1. Implementar una interfaz de escucha: 
+		 public class SwingApplication extends JFrame implements ActionListener 
+		 2. Agregar el oyente al botón (una vez creado) 
+		 button.addActionListener(this); 
+		 3. Definir los métodos de la interfaz de escucha: 
+		 
+		 
+		 
+		Public void actionPerformed (ActionEvent e) 
+		{ 
+		 numClicks ++; 
+		 label.setText (“Número de clics de botones: “ + numClicks); 
+		 }*/
 	
 }
 /**
