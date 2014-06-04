f93afb84bf5a89ba331790f77dfd1c3ae78e1663
jonathan <jonathan@jonathon>
2014-06-04 17:47:33 -0400

    g

---
+++ src/cl/paradigmas/gui/eventos/EventBuilder.java
@@ -0,0 +1,18 @@
+package cl.paradigmas.gui.eventos;
+
+
+
+final public class EventBuilder {
+private EventBuilder(){
+	
+	//button.addActionListener(this)
+	
+}
+/**
+* Aqui se deben crear los metodos static
+* que devolveran los ActionListener y
+* MouseAdapter->(MouseListener y MouseMotionListener)
+* que marcan los eventos
+*/
+
+}
